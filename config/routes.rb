43058a8bfcad6536563b490336e4df535c1dcc24
Thomas-Rudge <thomas.rudge85@gmail.com>
2016-11-07 17:02:22 +0000

    Added basic features
    User can see stored articles, and create new ones from the front-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :articles
+  
+  get 'articles/:id', to: 'articles#show', as: :article
+  
+  root 'articles#index'
 end
