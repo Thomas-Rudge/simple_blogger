066e0c359dabdc4dbec83bd4261019c7eb1fd5ab
Thomas-Rudge <thomas.rudge85@gmail.com>
2016-11-09 13:18:17 +0000

    Added article tags
    Articles can now be tagged when created, and users can use tags to see related articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,10 +20,13 @@ class ArticlesController < ApplicationController
   
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
+    
   end
   
   def edit
-    @article = Article.find(params[:format])
+    @article = Article.find(params[:id])
   end
   
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :articles
-  
-  get 'articles/:id', to: 'articles#show', as: :article
-  get 'articles/edit.:id', to: 'articles#edit', as: :edit
-  patch 'articles/:id', to: 'articles#update', as: :update
-  delete 'articles/:id', to: 'articles#destroy', as: :delete
-  
   root to: 'articles#index'
+  
+  resources :articles do
+    resources :comments
+  
+    get 'articles/:id', to: 'articles#show', as: :article
+    get 'articles/edit.:id', to: 'articles#edit', as: :edit
+    patch 'articles/:id', to: 'articles#update', as: :update
+    delete 'articles/:id', to: 'articles#destroy', as: :delete
+  end
+  
+  resources :tags
 end
